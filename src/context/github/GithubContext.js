27733a34391ff14ebb58cef91c17a3fc04ef0c23
Balafoutis Panagiotis <thrylos012@gmail.com>
2024-02-13 16:31:57 +0200

    first commit github-finder project

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,30 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+
+export const GithubProvider = ({ children }) => {
+	// const [users, setUsers] = useState([]);
+	// const [loading, setLoading] = useState(true);
+	const initialState = {
+		users: [],
+		user: {},
+		repos: [],
+		loading: false
+	};
+
+	const [state, dispatch] = useReducer(githubReducer, initialState);
+
+	return (
+		<GithubContext.Provider
+			value={{
+				...state,
+				dispatch
+			}}
+		>
+			{children}
+		</GithubContext.Provider>
+	);
+};
+
+export default GithubContext;
